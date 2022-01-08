8a942eae9de7ebe4a97e933bcf68c4933e29d7a8
timdev <timdj01@gmail.com>
2022-01-08 22:14:09 +0400

    Create Customer model, dto, controller and service

---
+++ customer/src/main/java/timdev/amigoscode/customer/Customer.java
@@ -0,0 +1,13 @@
+package timdev.amigoscode.customer;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Customer {
+	private Integer id;
+	private String firstName;
+	private String lastName;
+	private String email;
+}
---
+++ customer/src/main/java/timdev/amigoscode/customer/CustomerService.java
@@ -0,0 +1,18 @@
+package timdev.amigoscode.customer;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public record CustomerService() {
+
+	public void registerCustomer(CustomerRegistrationRequest customerRegistrationRequest) {
+		final Customer customer = Customer.builder()
+				.firstName(customerRegistrationRequest.firstName())
+				.lastName(customerRegistrationRequest.lastName())
+				.email(customerRegistrationRequest.email())
+				.build();
+		// todo: check if email valid
+		// todo: check if email not taken
+		// todo: store customer in db
+	}
+}
