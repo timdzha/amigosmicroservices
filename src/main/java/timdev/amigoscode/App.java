6bb5128098f2fde22cd6f2e0f8f9bf97db9dfe75
timdev <timdj01@gmail.com>
2022-01-08 21:08:27 +0400

    init

---
+++ src/main/java/timdev/amigoscode/App.java
@@ -0,0 +1,13 @@
+package timdev.amigoscode;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
