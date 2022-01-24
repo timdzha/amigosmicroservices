4da2d1d7ac75fc5fe2c499f01e66ae1b14cd9fb4
timdev <timdj01@gmail.com>
2022-01-24 23:03:45 +0400

    Config rabbit listener in amqp-ms

--- amqp/src/main/java/timdev/amigoscode/amqp/RabbitMqConfig.java
+++ amqp/src/main/java/timdev/amigoscode/amqp/RabbitMqConfig.java
@@ -2,12 +2,14 @@ package timdev.amigoscode.amqp;
 
 import lombok.AllArgsConstructor;
 import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 @Configuration
 @AllArgsConstructor
@@ -15,6 +17,10 @@ public class RabbitMqConfig {
 
 	private final ConnectionFactory connectionFactory;
 
+	/**
+	 * amqpTemplate to send message to the queue
+	 * @return
+	 */
 	@Bean
 	public AmqpTemplate amqpTemplate() {
 		final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
@@ -22,10 +28,27 @@ public class RabbitMqConfig {
 		return rabbitTemplate;
 	}
 
+	/**
+	 * listener to receive message from the queue
+	 * @return
+	 */
+	@Bean
+	public SimpleRabbitListenerContainerFactory factory() {
+		final SimpleRabbitListenerContainerFactory factory
+				= new SimpleRabbitListenerContainerFactory();
+		factory.setConnectionFactory(connectionFactory);
+		factory.setMessageConverter(jacksonConverter());
+		return factory;
+	}
+
+	/**
+	 * json message converter
+	 * we can use {@link Jackson2ObjectMapperBuilder#xml()} to convert json to xml
+	 * we can use {@link Jackson2ObjectMapperBuilder#json()} to convert json to json
+	 * @return
+	 */
 	@Bean
 	public MessageConverter jacksonConverter() {
-//		Jackson2ObjectMapperBuilder.json();
-//		Jackson2ObjectMapperBuilder.xml();
 		return new Jackson2JsonMessageConverter();
 	}
 }
