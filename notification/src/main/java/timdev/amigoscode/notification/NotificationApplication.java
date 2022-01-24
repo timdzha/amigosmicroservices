2f1841a04caef0c81a429851f207614e1d5da0ea
timdev <timdj01@gmail.com>
2022-01-25 00:08:42 +0400

    Check rabbitmq sending message, payload is string

--- notification/src/main/java/timdev/amigoscode/notification/NotificationApplication.java
+++ notification/src/main/java/timdev/amigoscode/notification/NotificationApplication.java
@@ -1,13 +1,34 @@
 package timdev.amigoscode.notification;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import timdev.amigoscode.amqp.RabbitMqMessageProducer;
 
-@SpringBootApplication
+@SpringBootApplication (
+		scanBasePackages = {
+				"timdev.amigoscode.notification",
+				"timdev.amigoscode.amqp"
+		}
+)
 @EnableEurekaClient
 public class NotificationApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(NotificationApplication.class, args);
 	}
+
+	@Bean
+	CommandLineRunner commandLineRunner(
+			RabbitMqMessageProducer producer,
+			NotificationConfig notificationConfig) {
+		return args -> {
+			producer.publish(
+					"foo",
+					notificationConfig.getInternalExchange(),
+					notificationConfig.getInternalNotificationRoutingKey());
+		};
+	}
+
 }
